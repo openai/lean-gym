9e4375d02dbb2dd5e1008e10dec70b18a1f98b66
Stanislas Polu <spolu@openai.com>
2021-06-01 12:49:43 +0200

    re-add list.nth_except

--- src/util/io.lean
+++ src/util/io.lean
@@ -31,3 +31,11 @@ meta def run_tactic'' {α} (tac :tactic α) : io α := do {
 
 end io
 end io
+
+-- convenience function for command-line argument parsing
+meta def list.nth_except {α} : list α → ℕ → string → io α := λ xs pos msg,
+  match (xs.nth pos) with
+  | (some result) := pure result
+  | none := do
+    io.fail' format!"must supply {msg} as argument {pos}"
+  end
