f4a94fcf54f8729be416ebdca82097d95a6f1c39
Stanislas Polu <spolu@openai.com>
2021-06-01 16:34:36 +0200

    miniF2F setup

---
+++ scripts/setup_miniF2F.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# run from project root
+
+rm -rf leanpkg.path
+rm -rf _target
+
+cp leanpkg_miniF2F.toml leanpkg.toml
+leanpkg configure
+mkdir ./_target/deps/minif2f/lean/scripts/
+cp ./_target/deps/mathlib/scripts/mk_all.sh ./_target/deps/minif2f/lean/scripts/mk_all.sh
+bash ./_target/deps/minif2f/lean/scripts/mk_all.sh
+cd _target/deps/minif2f && leanproject get-mathlib-cache && cd ../../../
+leanpkg build
