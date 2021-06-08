e724215490983b07869c544e9a4236361fa257f6
Stanislas Polu <spolu@openai.com>
2021-06-08 18:18:05 +0200

    Remove extraneous leanproject get mathlib

--- scripts/setup_miniF2F.sh
+++ scripts/setup_miniF2F.sh
@@ -11,5 +11,5 @@ mkdir ./_target/deps/minif2f/lean/scripts/
 cp ./_target/deps/mathlib/scripts/mk_all.sh ./_target/deps/minif2f/lean/scripts/mk_all.sh
 bash ./_target/deps/minif2f/lean/scripts/mk_all.sh
 leanproject get-mathlib-cache
-cd _target/deps/minif2f && leanproject get-mathlib-cache && cd ../../../
+# cd _target/deps/minif2f && leanproject get-mathlib-cache && cd ../../../
 leanpkg build
