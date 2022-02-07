e3a19642c1d3ee3dbf494177f1e9ec1a1ef032b4
Stanislas Polu <spolu@openai.com>
2022-02-07 11:49:30 +0100

    more tests, clean-up

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # run from project root
+
 leanpkg configure
 bash ./_target/deps/mathlib/scripts/mk_all.sh
 leanproject get-mathlib-cache
--- tests/test_validate_proof.lean
+++ tests/test_validate_proof.lean
@@ -1,6 +1,7 @@
 import util.tactic
+import data.real.basic
 
-meta def should_fail
+meta def should_fail_1
   (u v : ℕ+)
   (h₀ : 100 ∣ (14*u - 46))
   (h₁ : 100 ∣ (14*v - 46))
@@ -15,11 +16,48 @@ begin
   exact not_lt_of_lt hn undefined,
 end
 
-meta def should_succeed : true := by trivial
+meta def should_fail_2
+  (p q : ℝ → ℝ)
+  (h₀ : ∀ x, p x = 2 - x^2)
+  (h₁ : ∀ x≠0, q x = 6 / x) :
+  p (q 2) = -7 :=
+begin
+  sorry,
+end
+
+-- meta def should_fail_3
+--   (n : ℕ) :
+--   3 ∣ n^3 + 2 * n :=
+-- begin
+--   rw [add_comm],
+--   have h3 : 1 * (n + 1) ≤ (n + 1),
+--   rw one_mul,
+--   apply dvd_trans,
+--   swap,
+--   simp [],
+-- end
+
+meta def should_succeed_1 : true := by trivial
+
+meta def should_succeed_2
+  (p q : ℝ → ℝ)
+  (h₀ : ∀ x, p x = 2 - x^2)
+  (h₁ : ∀ x≠0, q x = 6 / x) :
+  p (q 2) = -7 :=
+begin
+  rw [h₀, h₁],
+  ring,
+  linarith,
+end
+
 
 meta def main : io unit := io.run_tactic'' $ do {
   (tactic.guard_undefined `(undefined : ℕ) <|> tactic.trace "OK"),
   (tactic.guard_sorry `(sorry : ℕ) <|> tactic.trace "OK"),
-  (validate_decl `should_fail) <|> tactic.trace "OK",
-  validate_decl `should_succeed *> tactic.trace "OK"
+
+  (validate_decl `should_fail_1) <|> tactic.trace "OK",
+  (validate_decl `should_fail_2) <|> tactic.trace "OK",
+  -- (validate_decl `should_fail_3) <|> tactic.trace "OK",
+  validate_decl `should_succeed_1 *> tactic.trace "OK",
+  validate_decl `should_succeed_2 *> tactic.trace "OK"
 }
