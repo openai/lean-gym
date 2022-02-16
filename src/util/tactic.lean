6818de953da9cc45e715b51bc650875d735b0599
Daniel Selsam <dselsam@openai.com>
2022-02-15 16:34:44 -0800

    refactor: remove tactic state post processing

--- src/util/tactic.lean
+++ src/util/tactic.lean
@@ -105,35 +105,10 @@ setup_tactic_parser
 meta def num_goals' : tactic_state → option ℕ :=
 λ ts, match tactic.num_goals ts with | (success val _) := pure val | _ := none end
 
--- TODO(jesse): this is a hack. might be better to do this in python
-meta def consume_with_parser {α} (p : lean.parser α) : string → io string := λ inp, do {
-  io.run_tactic'' $ do
-    prod.snd <$> lean.parser.run (with_input p inp)
-}
-
--- TODO(jesse): performance
-meta def consume_spaces : string → string
-| arg@⟨[]⟩ := arg
-| arg@⟨(x::xs)⟩ := if x = ' ' then consume_spaces ⟨xs⟩ else arg
-
--- WARNING: this is a hack
-meta def remove_indents_with_split (c : char := '\t'): string → string := λ str,
-let strs := str.split (= '\t') in
-string.intercalate (⟨['\t']⟩ : string) (consume_spaces <$> strs)
-
-meta def postprocess_tactic_state : tactic_state → tactic string := λ ts, do
-  let main : tactic string := do {
-    let ts_str := "\\\"".intercalate (ts.to_format.to_string.split (= '"')),
-    tabbed_ts_str ← do {
-      if (num_goals' ts).get_or_else 0 ≤ 1
-      then pure $ ts_str.replace_char '\n' '\t'
-      else tactic.unsafe_run_io $ (λ x, string.replace_char x '\n' '\t')
-             <$> (consume_with_parser small_nat >=>
-               consume_with_parser ident) ts_str},
-    pure $ remove_indents_with_split '\t' tabbed_ts_str
-  },
-  main <|> (let msg := "[postprocess_tactic_state] WARNING: POSTPROCESSING FAILED" in
-    tactic.trace msg *> tactic.fail msg)
+meta def postprocess_tactic_state (ts : tactic_state) : tactic string := do
+  -- Note: we do not postprocess here, because we assume that there are other
+  -- data sources that use default `pp` settings.
+  pure $ to_string (to_fmt ts)
 
 end tactic_state
 
