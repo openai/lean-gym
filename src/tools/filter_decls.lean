958e80ffb48c1f781eaada79d75f3b45c6cdc2eb
Daniel Selsam <dselsam@openai.com>
2022-02-15 17:12:17 -0800

    refactor: update filter_decls to deal with new decl metadata format

--- src/tools/filter_decls.lean
+++ src/tools/filter_decls.lean
@@ -17,34 +17,37 @@ end io
 
 setup_tactic_parser
 
-meta def parse_decl_nm_and_open_ns : string → tactic (name × list name) := λ inp,
-flip lean.parser.run_with_input inp $ prod.mk <$> iterate_until ident (λ nm, pure ∘ bnot $ nm = name.anonymous) 100 <*> many ident
+-- Expected file format:
+-- <decl>, <import> ... <import>, <open> ... <open>
 
--- #eval (["foo bar baz\n", "baz baz baz\n", "a b c d e\n"].mmap $ io.run_tactic' ∘ parse_decl_nm_and_open_ns) >>= λ x, io.put_str_ln' format!"{x}"
+meta def parse_decl_and_metadata (input : string) : tactic (name × list name × list name) := do
+ flip lean.parser.run_with_input input $ do
+  name ← ident,
+  tk ",",
+  imports ← many ident,
+  tk ",",
+  opens ← many ident,
+  pure (name, imports, opens)
 
 end
 
 def for_ {m α β} [monad m] (xs : list α) (body : α → m β) := list.mmap' body xs
-
 
 meta def main_aux (names_file : string) (dest : string) : io unit := do {
   nm_strs ← (io.mk_file_handle names_file io.mode.read >>= λ f,
     (string.split (λ c, c = '\n') <$> buffer.to_string <$> io.fs.read_to_end f)),
 
   let nm_strs := nm_strs.filter (λ x : string, x.length > 0),
-  nms : list (name × list name) ← io.run_tactic'' $ nm_strs.mmap parse_decl_nm_and_open_ns,
+  nms : list (name × list name × list name) ← io.run_tactic'' $ nm_strs.mmap parse_decl_and_metadata,
   dest_handle ← io.mk_file_handle dest io.mode.write,
 
   io.run_tactic'' $ do {
     env ← tactic.get_env,
-    for_ nms $ λ ⟨nm, open_ns⟩, tactic.try $ do {
+    for_ (nm_strs.zip nms) $ λ ⟨nm_str, ⟨nm, imports, open_ns⟩⟩, tactic.try $ do {
       decl ← env.get nm,
       if decl.is_theorem then do {
         tactic.trace format! "[filter_defs] KEEPING {nm.to_string}",
-        tactic.unsafe_run_io $
-          io.fs.put_str_ln_flush
-            dest_handle
-              (nm.to_string ++ " " ++ (" ".intercalate $ name.to_string <$> open_ns))
+        tactic.unsafe_run_io $ io.fs.put_str_ln_flush dest_handle nm_str
       } else do {
         tactic.trace format! "[filter_defs] DISCARDING {nm.to_string}",
         pure ()
