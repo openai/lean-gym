2ebaf0dc909c7bcb03a8477a708345aea6e13dc4
Stanislas Polu <spolu@openai.com>
2022-02-17 14:03:03 +0100

    fix minif2f steup

--- scripts/setup_miniF2F.sh
+++ scripts/setup_miniF2F.sh
@@ -7,8 +7,11 @@ rm -rf _target
 
 cp leanpkg_miniF2F.toml leanpkg.toml
 leanpkg configure
-mkdir ./_target/deps/minif2f/lean/scripts/
-cp ./_target/deps/mathlib/scripts/mk_all.sh ./_target/deps/minif2f/lean/scripts/mk_all.sh
-bash ./_target/deps/minif2f/lean/scripts/mk_all.sh
+bash ./_target/deps/mathlib/scripts/mk_all.sh
+
+mv ./_target/deps/mathlib/src/all.lean ./_target/deps/minif2f/lean/src/all.lean
+echo "import test" >> ./_target/deps/minif2f/lean/src/all.lean
+echo "import valid" >> ./_target/deps/minif2f/lean/src/all.lean
+
 leanproject get-mathlib-cache
 leanpkg build
